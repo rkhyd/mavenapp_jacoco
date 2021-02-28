24c4deedddb36e2d1a79883838ada3891e495132
ravikumar <ravikumar@gratian.tech>
2021-02-28 19:56:01 +0530

    mavenapp files with jacoco in pom.xml

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,26 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+    
+    public static int calc(int a, int b) {
+        int result = a * b;
+        return  result;
+    }
+    
+    private final String getMessage() {
+        return message;
+    }
+
+}
